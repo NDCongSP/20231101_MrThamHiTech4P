0c8623f7db3b1a739dda0e24c9536bfc468555f0
congnguyen-framas <68525123+congnguyen-framas@users.noreply.github.com>
2023-11-24 16:39:59 +0700

    up

---
+++ 20231105_MrThamTraiHeoTinhVinhPhuc11/sourceCode/WebApplication1/WebApplication1/Pages/LoginPage.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages
+{
+    public partial class LoginPage
+    {
+        public TokenRequest _model = new TokenRequest();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        private void HandleLogin()
+        {
+
+        }
+    }
+}
---
+++ 20231105_MrThamTraiHeoTinhVinhPhuc11/sourceCode/WebApplication1/WebApplication1/Shared/LoginLayout.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Shared
+{
+    public partial class LoginLayout
+    {
+        bool _drawerOpen = true;
+
+        void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+
+        void BtnMenuItemProfileClick()
+        {
+            _navigationManager.NavigateTo("/login");
+        }
+
+        void BtnMenuItemSignOutClick()
+        {
+            //_authService.Logout();
+            _navigationManager.NavigateTo("/login");
+        }
+    }
+}
