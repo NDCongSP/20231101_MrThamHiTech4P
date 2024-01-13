04376fdba0a72808f34b683c0c10df5884a255d2
NDCongSP <ndcong08cddv02@gmail.com>
2024-01-13 09:14:14 +0700

    UP OK

---
+++ 20231105_VinhPhuc1/sourceCode/GiamSat.API/BlazorApp2/Pages/Trend.razor.cs
@@ -0,0 +1,146 @@
+﻿using AntDesign.Charts;
+using Title = AntDesign.Charts.Title;
+using System.Diagnostics;
+using System;
+using System.Reflection.Emit;
+using System.Timers;
+using AntDesign;
+
+namespace BlazorApp2.Pages
+{
+    public partial class Trend
+    {
+        IChartComponent chart1;
+        IChartComponent chart5;
+
+        Line lineChart = new Line();
+
+        private System.Timers.Timer _timer;
+        private bool _timerTick = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            //await chart1.ChangeData(data1);
+
+
+            await base.OnInitializedAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                lineChart = new Line();
+                lineChart.Config = config3;
+                lineChart.Data = data3;
+                
+
+                #region Timer refresh data
+            _timer = new System.Timers.Timer(1000);
+                _timer.Elapsed += RefreshData;
+                _timer.Enabled = true;
+                #endregion
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        private async void RefreshData(object? sender, ElapsedEventArgs e)
+        {
+            //try
+            {
+                await AddDataAsync();
+
+                _timerTick = false;
+
+                StateHasChanged(); // NOTE: MUST CALL StateHasChanged() BECAUSE THIS IS TRIGGERED BY A TIMER INSTEAD OF A USER EVENT
+            }
+            ///catch { _timerTick = false; }
+        }
+
+        private async Task AddDataAsync()
+        {
+            //try
+            {
+                if (!_timerTick)
+                {
+                    _timerTick = true;
+
+                    //data3.AddIf(true, () => new { year = $"{DateTime.Now.ToString("HH:mm:ss")}", Type = "S1", value = 3 });
+                    //data3.AddIf(true, () => new { year = $"{DateTime.Now.ToString("HH:mm:ss")}", Type = "S2", value = 7 });
+
+                    var d=data3.Count();
+
+                    data3.Remove(0);
+                    data3.Remove(1);
+
+                    var d1 = data3.Count();
+
+                   
+                }
+            }
+            //catch { }
+        }
+
+        #region 示例3
+
+        object[] data3 =
+                        {
+                            new {year = $"{DateTime.Now.ToString("HH:mm:ss")}", Type="S1", value = 3},
+                            new {year =  $"{DateTime.Now.ToString("HH:mm:ss")}", Type="S2", value = 5},
+                            new {year = $"{DateTime.Now.AddSeconds(10).ToString("HH:mm:ss")}", Type="S1", value = 7},
+                            new {year =  $"{DateTime.Now.AddSeconds(10).ToString("HH:mm:ss")}", Type="S2", value = 9},
+                            new {year = $"{DateTime.Now.AddSeconds(20).ToString("HH:mm:ss")}", Type="S1", value = 5},
+                            new {year =  $"{DateTime.Now.AddSeconds(20).ToString("HH:mm:ss")}", Type="S2", value = 10},
+                        };
+
+        [Obsolete]
+        LineConfig config3 = new LineConfig
+        {
+            Title = new Title
+            {
+                Visible = true,
+                Text = "test"
+            },
+            Description = new Description
+            {
+                Visible = true,
+                Text = "Mo ta"
+            },
+            ForceFit = true,
+            Padding = "auto",
+            XField = "year",
+            YField = "value",
+            //Smooth = true,
+            //YAxis = new ValueAxis
+            //{
+            //    Label = new BaseAxisLabel()
+            //},
+            Legend = new Legend
+            {
+                Position = "right-top"
+            },
+
+            Point = new LineViewConfigPoint
+            {
+                Visible = true,
+                Size = 5,
+                Shape = "diamond",
+                Style = new GraphicStyle
+                {
+                    Fill = "white",
+                    Stroke = "#2593fc",
+                    LineWidth = 2
+                }
+            },
+            Label = new AntDesign.Charts.Label
+            {
+                Visible = true,
+                Type = "point"
+            },
+            SeriesField = "type",
+            Color = new string[] { "#1979C9", "#D62A0D" },
+            Responsive = true,
+        };
+        #endregion 示例3
+    }
+}
--- 20231105_VinhPhuc1/sourceCode/GiamSat.API/GiamSat.UI/Pages/Trend.razor.cs
+++ 20231105_VinhPhuc1/sourceCode/GiamSat.API/GiamSat.UI/Pages/Trend.razor.cs
@@ -3,6 +3,8 @@ using Blazorise;
 using GiamSat.Models;
 using MudBlazor;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Timers;
 
 namespace GiamSat.UI.Pages
 {
@@ -20,9 +22,12 @@ namespace GiamSat.UI.Pages
         private List<APIClient.RealtimeDisplayModel>? _dataFromDB;
         private RealtimeList? _displayRealtime;
 
+        private System.Timers.Timer _timer;
+        private bool _timerTick = false;
+
         protected override async Task OnInitializedAsync()
         {
-            
+
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
@@ -30,7 +35,15 @@ namespace GiamSat.UI.Pages
             if (firstRender)
             {
                 await InitialChart();
-                await lineChart.InitializeAsync(chartData: chartData, chartOptions: lineChartOptions, plugins: new string[] { "ChartDataLabels" });
+                await lineChart.InitializeAsync(chartData: chartData, chartOptions: lineChartOptions);
+                ////hiển thị value tại mỗi điểm
+                //await lineChart.InitializeAsync(chartData: chartData, chartOptions: lineChartOptions, plugins: new string[] { "ChartDataLabels" });
+
+                #region Timer refresh data
+                _timer = new System.Timers.Timer(1000);
+                _timer.Elapsed += RefreshData;
+                _timer.Enabled = true;
+                #endregion
             }
 
             await base.OnAfterRenderAsync(firstRender);
@@ -39,9 +52,9 @@ namespace GiamSat.UI.Pages
         private async Task InitialChart()
         {
             var colors = ColorBuilder.CategoricalTwelveColors;
-            var labels = new List<string> { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            var labels = new List<string> { DateTime.Now.ToString("HH:mm:ss") };
             var datasets = new List<IChartDataset>();
-           
+
 
             lineChartOptions = new();
             lineChartOptions.Responsive = true;
@@ -56,9 +69,9 @@ namespace GiamSat.UI.Pages
             lineChartOptions.Plugins.Title.Text = "Đồ thị nhiệt độ";
             lineChartOptions.Plugins.Title.Display = true;
 
-            // datalabels
-            lineChartOptions.Plugins.Datalabels.Color = "white";
-            lineChartOptions.Plugins.Datalabels.Font = new LineChartDataLabelsFont() { Weight = "Size.Small" };
+            //// datalabels
+            //lineChartOptions.Plugins.Datalabels.Color = "white";
+            //lineChartOptions.Plugins.Datalabels.Font = new LineChartDataLabelsFont() { Weight = "Size.Small" };
 
             try
             {
@@ -75,6 +88,30 @@ namespace GiamSat.UI.Pages
                     }
 
                     _displayRealtime = JsonConvert.DeserializeObject<RealtimeList>(_dataFromDB.FirstOrDefault().DisplayData);
+
+                    int index = 0;
+
+                    foreach (var item in _displayRealtime)
+                    {
+                        var dataset = new LineChartDataset
+                        {
+                            Label = $"{item.DisplayRealtime.TenChuong}",
+                            Data = new List<double> { item.DisplayRealtime.Temperature },
+                            BackgroundColor = new List<string> { colors[index] },
+                            BorderColor = new List<string> { colors[index] },
+                            BorderWidth = new List<double> { 2 },
+                            HoverBorderWidth = new List<double> { 4 },
+                            PointBackgroundColor = new List<string> { colors[index] },
+                            PointRadius = new List<int> { 3 }, // hide points
+                            PointHoverRadius = new List<int> { 4 },
+                            PointStyle=new List<string> { "diamond" },//diamond
+                            // datalabels
+                            //Datalabels = new() { Align = "end", Anchor = "end" },
+
+                        };
+                        datasets.Add(dataset);
+                        index += 1;
+                    }
                 }
                 else
                 {
@@ -89,51 +126,53 @@ namespace GiamSat.UI.Pages
                 _snackBar.Add(ex.Message, Severity.Error);
             }
 
-            var dataset1 = new LineChartDataset
-            {
-                Label = "Windows",
-                Data = new List<double> { 7265791, 5899643, 6317759, 6315641, 5338211, 8496306, 7568556, 8538933, 8274297, 8657298, 7548388, 7764845 },
-                BackgroundColor = new List<string> { colors[0] },
-                BorderColor = new List<string> { colors[0] },
-                BorderWidth = new List<double> { 2 },
-                HoverBorderWidth = new List<double> { 4 },
-                PointBackgroundColor = new List<string> { colors[0] },
-                PointRadius = new List<int> { 0 }, // hide points
-                PointHoverRadius = new List<int> { 4 },
+            #region add datasheet
+            //var dataset1 = new LineChartDataset
+            //{
+            //    Label = "Windows",
+            //    Data = new List<double> { 7265791, 5899643, 6317759, 6315641, 5338211, 8496306, 7568556, 8538933, 8274297, 8657298, 7548388, 7764845 },
+            //    BackgroundColor = new List<string> { colors[0] },
+            //    BorderColor = new List<string> { colors[0] },
+            //    BorderWidth = new List<double> { 2 },
+            //    HoverBorderWidth = new List<double> { 4 },
+            //    PointBackgroundColor = new List<string> { colors[0] },
+            //    PointRadius = new List<int> { 0 }, // hide points
+            //    PointHoverRadius = new List<int> { 4 },
 
-                // datalabels
-                Datalabels = new() { Align = "end", Anchor = "end"},
+            //    // datalabels
+            //    Datalabels = new() { Align = "end", Anchor = "end"},
 
-            };
-            datasets.Add(dataset1);
+            //};
+            //datasets.Add(dataset1);
 
-            var dataset2 = new LineChartDataset
-            {
-                Label = "macOS",
-                Data = new List<double> { 1809499, 1816642, 2122410, 1809499, 1850793, 1846743, 1954797, 2391313, 1983430, 2469918, 2633303, 2821149 },
-                BackgroundColor = new List<string> { colors[1] },
-                BorderColor = new List<string> { colors[1] },
-                BorderWidth = new List<double> { 2 },
-                HoverBorderWidth = new List<double> { 4 },
-                PointBackgroundColor = new List<string> { colors[1] },
-                PointRadius = new List<int> { 0 }, // hide points
-                PointHoverRadius = new List<int> { 4 }
-            };
-            datasets.Add(dataset2);
+            //var dataset2 = new LineChartDataset
+            //{
+            //    Label = "macOS",
+            //    Data = new List<double> { 1809499, 1816642, 2122410, 1809499, 1850793, 1846743, 1954797, 2391313, 1983430, 2469918, 2633303, 2821149 },
+            //    BackgroundColor = new List<string> { colors[1] },
+            //    BorderColor = new List<string> { colors[1] },
+            //    BorderWidth = new List<double> { 2 },
+            //    HoverBorderWidth = new List<double> { 4 },
+            //    PointBackgroundColor = new List<string> { colors[1] },
+            //    PointRadius = new List<int> { 0 }, // hide points
+            //    PointHoverRadius = new List<int> { 4 }
+            //};
+            //datasets.Add(dataset2);
 
-            var dataset3 = new LineChartDataset
-            {
-                Label = "Other",
-                Data = new List<double> { 1081241, 1100363, 1118136, 1073255, 1120315, 1395736, 1488788, 1489466, 1489947, 1414739, 1735811, 1820171 },
-                BackgroundColor = new List<string> { colors[2] },
-                BorderColor = new List<string> { colors[2] },
-                BorderWidth = new List<double> { 2 },
-                HoverBorderWidth = new List<double> { 4 },
-                PointBackgroundColor = new List<string> { colors[2] },
-                PointRadius = new List<int> { 0 }, // hide points
-                PointHoverRadius = new List<int> { 4 }
-            };
-            datasets.Add(dataset3);
+            //var dataset3 = new LineChartDataset
+            //{
+            //    Label = "Other",
+            //    Data = new List<double> { 1081241, 1100363, 1118136, 1073255, 1120315, 1395736, 1488788, 1489466, 1489947, 1414739, 1735811, 1820171 },
+            //    BackgroundColor = new List<string> { colors[2] },
+            //    BorderColor = new List<string> { colors[2] },
+            //    BorderWidth = new List<double> { 2 },
+            //    HoverBorderWidth = new List<double> { 4 },
+            //    PointBackgroundColor = new List<string> { colors[2] },
+            //    PointRadius = new List<int> { 0 }, // hide points
+            //    PointHoverRadius = new List<int> { 4 }
+            //};
+            //datasets.Add(dataset3);
+            #endregion
 
             chartData = new ChartData
             {
@@ -142,56 +181,108 @@ namespace GiamSat.UI.Pages
             };
         }
 
+        private async void RefreshData(object? sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                await AddDataAsync();
+
+                _timerTick = false;
+
+                StateHasChanged(); // NOTE: MUST CALL StateHasChanged() BECAUSE THIS IS TRIGGERED BY A TIMER INSTEAD OF A USER EVENT
+            }
+            catch { _timerTick = false; }
+        }
+
         private async Task AddDatasetAsync()
         {
             if (chartData is null || chartData.Datasets is null) return;
 
-            var chartDataset = GetRandomLineChartDataset();
+            var chartDataset = GetLineChartDataset();
             chartData = await lineChart.AddDatasetAsync(chartData, chartDataset, lineChartOptions);
         }
 
         private async Task AddDataAsync()
         {
-            if (chartData is null || chartData.Datasets is null)
-                return;
-
-            var data = new List<IChartDatasetData>();
-            foreach (var dataset in chartData.Datasets)
+            try
             {
-                if (dataset is LineChartDataset lineChartDataset)
-                    data.Add(new LineChartDatasetData(lineChartDataset.Label, random.Next(200)));
+                if (!_timerTick)
+                {
+                    _timerTick = true;
+
+                    if (chartData is null || chartData.Datasets is null)
+                    {
+                        return;
+                    }
+
+                    var res = await _realtimeDisplayClient.GetAllAsync().ConfigureAwait(true);
+
+                    if (res.Succeeded)
+                    {
+                        _dataFromDB = res.Data.ToList();
+
+                        if (_dataFromDB == null && _dataFromDB.Count <= 0)
+                        {
+                            _snackBar.Add("Data Null", Severity.Warning);
+                            return;
+                        }
+
+                        _displayRealtime = JsonConvert.DeserializeObject<RealtimeList>(_dataFromDB.FirstOrDefault().DisplayData);
+
+                        //add data to chart
+                        //var data = new List<IChartDatasetData>();
+                        //foreach (var item in _displayRealtime)
+                        //{
+                        //    foreach (var dataset in chartData.Datasets)
+                        //    {
+                        //        if (dataset is LineChartDataset lineChartDataset)
+                        //        {
+                        //            data.Add(new LineChartDatasetData(lineChartDataset.Label, random.Next(200)));
+                        //        }
+                        //    }
+                        //}
+
+                        //chartData = await lineChart.AddDataAsync(chartData, DateTime.Now.ToString("HH:mm:ss"), data);
+                    }
+
+
+                    //add data to chart
+                    var data = new List<IChartDatasetData>();
+                    foreach (var dataset in chartData.Datasets)
+                    {
+                        if (dataset is LineChartDataset lineChartDataset)
+                        {
+                            //var ds = dataset as LineChartDataset;                         
+
+                            var count = lineChartDataset.Data.Count;
+                            Debug.WriteLine($"Count point: {count}");
+
+                            if (count > 10)
+                            {
+                                lineChartDataset.Data.RemoveAt(0);
+                            }
+                            data.Add(new LineChartDatasetData(lineChartDataset.Label, random.Next(30)));
+                        }
+                    }
+
+                    if (chartData.Labels.Count > 10)
+                    {
+                        chartData.Labels.RemoveAt(0);
+
+                        await lineChart.UpdateAsync(chartData, lineChartOptions);
+                    }
+
+                    //await lineChart.UpdateAsync(chartData, lineChartOptions);
+
+                    await lineChart.AddDataAsync(chartData, DateTime.Now.ToString("HH:mm:ss"), data);
+                }
             }
-
-            chartData = await lineChart.AddDataAsync(chartData, GetNextDataLabel(), data);
-        }
-
-        private async Task ShowHorizontalLineChartAsync()
-        {
-            lineChartOptions.IndexAxis = "y";
-            await lineChart.UpdateAsync(chartData, lineChartOptions);
-        }
-
-        private async Task ShowVerticalLineChartAsync()
-        {
-            lineChartOptions.IndexAxis = "x";
-            await lineChart.UpdateAsync(chartData, lineChartOptions);
+            catch { }
         }
 
         #region Data Preparation
 
-        private List<IChartDataset> GetDefaultDataSets(int numberOfDatasets)
-        {
-            var datasets = new List<IChartDataset>();
-
-            for (var index = 0; index < numberOfDatasets; index++)
-            {
-                datasets.Add(GetRandomLineChartDataset());
-            }
-
-            return datasets;
-        }
-
-        private LineChartDataset GetRandomLineChartDataset()
+        private LineChartDataset GetLineChartDataset()
         {
             var c = ColorBuilder.CategoricalTwelveColors[datasetsCount].ToColor();
 
@@ -220,17 +311,6 @@ namespace GiamSat.UI.Pages
             }
 
             return data;
-        }
-
-        private List<string> GetDefaultDataLabels(int numberOfLabels)
-        {
-            var labels = new List<string>();
-            for (var index = 0; index < numberOfLabels; index++)
-            {
-                labels.Add(GetNextDataLabel());
-            }
-
-            return labels;
         }
 
         private string GetNextDataLabel()
